fc6dc5e674edf4a7a56d1caa63fdbf556b7d8b47
Artem <hartem472@gmail.com>
2024-09-12 12:28:46 +0200

    Set up User Budget and User Dashboard pages

---
+++ src/app/app/budget/page.tsx
@@ -0,0 +1,5 @@
+import { Typography } from '@mui/material';
+
+export default function BudgetPage() {
+  return <Typography>I am Budget page placeholder</Typography>;
+}
---
+++ src/app/app/page.tsx
@@ -0,0 +1,5 @@
+import { Typography } from '@mui/material';
+
+export default function BudgetPage() {
+  return <Typography>I am Dashboard page placeholder</Typography>;
+}
