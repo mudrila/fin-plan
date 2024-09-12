5527578c3141cee49336b6ec4d2096812467f990
Artem <hartem472@gmail.com>
2024-09-12 10:09:49 +0200

    run pretty

--- auth.ts
+++ auth.ts
@@ -1,8 +1,8 @@
-import NextAuth from "next-auth"
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { prisma } from "./prisma" 
- 
+import NextAuth from 'next-auth';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { prisma } from './prisma';
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [],
-})
+});
