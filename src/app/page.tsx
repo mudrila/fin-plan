79e5aadacb0ccb53ccd512d25f4aaed9169d069c
Kyrylo Klymenko <klimenkomud@gmail.com>
2024-10-05 21:36:14 +0200

    Use proper app name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Typography } from '@mui/material';
+import { APP_NAME } from '@/constants/content';
 
-export default function Home() {
-  return <Typography>FinPlan UI</Typography>;
+export default function LandingPage() {
+  return <Typography>{APP_NAME} - Landing Placeholder</Typography>;
 }
