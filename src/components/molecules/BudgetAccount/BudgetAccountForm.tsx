e575e0d4b707d879d727946791cf3a40a6250396
Artem <hartem472@gmail.com>
2024-10-09 13:11:31 +0200

    extract all the repeatable logic / UI into single component

--- src/components/molecules/BudgetAccount/BudgetAccountForm.tsx
+++ src/components/molecules/BudgetAccount/BudgetAccountForm.tsx
@@ -1,15 +1,9 @@
 'use client';
 
 import AddIcon from '@mui/icons-material/Add';
-import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import EditIcon from '@mui/icons-material/Edit';
 import {
-  Box,
   Button,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogTitle,
   FormControl,
   IconButton,
   InputLabel,
@@ -17,9 +11,8 @@ import {
   Select,
   TextField,
 } from '@mui/material';
-import { useRouter } from 'next/navigation';
-import { FormEvent, useState, useTransition } from 'react';
-import { toast } from 'sonner';
+import { useState } from 'react';
+import MainForm from './Form';
 import IconSelect from '@/components/molecules/IconSelect/IconSelect';
 import { accountTypes } from '@/constants/content';
 import { SerializedBudgetAccount } from '@/types/budget';
@@ -29,240 +22,131 @@ export default function BudgetAccountForm({
 }: {
   account?: SerializedBudgetAccount | null;
 }) {
-  const [open, setOpen] = useState(false);
   const [title, setTitle] = useState(account?.title || '');
   const [description, setDescription] = useState(account?.description || '');
   const [monthlyLimit, setMonthlyLimit] = useState(account?.monthlyLimit?.toString() || '0');
   const [type, setType] = useState(account?.type.toString() || 'Debit');
   const [icon, setIcon] = useState(account?.icon || '');
-  const [currentBalance, setCurrentBalance] = useState(account?.currentBalance?.toString() || '');
-  const [isPending, startTransition] = useTransition();
-  const router = useRouter();
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  const handleDelete = (event: FormEvent) => {
-    event.preventDefault();
-    const loadingToastId = toast.loading(`Hand tight - we are deleting ${account?.title}...`);
-
-    startTransition(async () => {
-      try {
-        const response = await fetch(`/api/budgetAccount/${account?.id}`, { method: 'DELETE' });
-        const responseData = await response.json();
-
-        if (responseData.errorMessage) {
-          toast.error(responseData.errorMessage, { id: loadingToastId });
-        } else if (responseData.success) {
-          router.refresh();
-          toast.success('Budget Account deleted!', { duration: 5000, id: loadingToastId });
-          handleClose();
-        } else {
-          toast.error('Something went wrong...', { id: loadingToastId });
-        }
-      } catch (e) {
-        console.error('Error while deletion budget account', e);
-        toast.error('Something went wrong...', { id: loadingToastId });
-      }
-    });
-  };
-
-  const handleSubmit = async (event: FormEvent) => {
-    event.preventDefault();
-    const loadingToastId = toast.loading(
-      `Hand tight - we are ${account ? 'updating' : 'creating'} budget account for ya...`,
-    );
-
-    startTransition(async () => {
-      try {
-        const response = await fetch(
-          account ? `/api/budgetAccount/${account.id}` : '/api/budgetAccount',
-          {
-            method: account ? 'PUT' : 'POST',
-            body: JSON.stringify({
-              title,
-              description,
-              icon,
-              monthlyLimit,
-              type,
-              currentBalance,
-            }),
-          },
-        );
-        const responseData = await response.json();
-
-        if (responseData.errorMessage) {
-          toast.error(responseData.errorMessage, { id: loadingToastId });
-        } else if (responseData.success) {
-          router.refresh();
-          toast.success(
-            `Budget Account ${account?.title ? account.title : title} ${account ? 'updated' : 'created'}!`,
-            {
-              duration: 5000,
-              id: loadingToastId,
-            },
-          );
-          handleClose();
-        } else {
-          toast.error('Something went wrong...', { id: loadingToastId });
-        }
-      } catch (e) {
-        console.error('Error while creating / updating budget account', e);
-        toast.error('Something went wrong...', { id: loadingToastId });
-      }
-    });
-  };
+  const [currentBalance, setCurrentBalance] = useState(account?.currentBalance?.toString() || '0');
 
   return (
     <>
-      {account ? (
-        <IconButton
-          onClick={handleClickOpen}
-          aria-label="edit"
-          color="primary"
-        >
-          <EditIcon />
-        </IconButton>
-      ) : (
-        <Button
-          variant="outlined"
-          onClick={handleClickOpen}
-          endIcon={<AddIcon />}
-        >
-          Budget Account
-        </Button>
-      )}
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        PaperProps={{
-          component: 'form',
-          onSubmit: handleSubmit,
-        }}
-      >
-        <DialogTitle>{account ? `Edit ${account.title}` : 'Create Budget Account'}</DialogTitle>
-        <DialogContent>
-          <TextField
-            autoFocus
-            required
-            margin="dense"
-            id="title"
-            name="title"
-            label="Title"
-            type="title"
-            fullWidth
-            variant="outlined"
-            value={title}
-            onChange={e => setTitle(e.target.value)}
-            disabled={isPending}
-          />
-          <TextField
-            id="description"
-            name="description"
-            margin="dense"
-            label="Description (Optional)"
-            type="text"
-            fullWidth
-            variant="outlined"
-            value={description}
-            onChange={e => setDescription(e.target.value)}
-            disabled={isPending}
-          />
-          <IconSelect
-            icon={icon}
-            setIcon={setIcon}
-          />
-          <TextField
-            id="currentBalance"
-            name="currentBalance"
-            margin="dense"
-            label="Current Balance"
-            type="number"
-            fullWidth
-            variant="outlined"
-            defaultValue={0}
-            required
-            value={currentBalance}
-            onChange={e => setCurrentBalance(e.target.value)}
-            disabled={isPending}
-          />
-          <TextField
-            id="monthlyLimit"
-            name="monthlyLimit"
-            margin="dense"
-            label="Monthly Limit"
-            type="number"
-            fullWidth
-            variant="outlined"
-            defaultValue={0}
-            required
-            value={monthlyLimit}
-            onChange={e => setMonthlyLimit(e.target.value)}
-            disabled={isPending}
-          />
-          <FormControl
-            fullWidth
-            margin="dense"
-            variant="outlined"
-          >
-            <InputLabel id="type-label">Type</InputLabel>
-            <Select
-              id="type"
-              name="type"
-              labelId="type-label"
-              label="Type"
-              defaultValue="Debit"
-              required
-              value={type}
-              onChange={e => setType(e.target.value)}
-              disabled={isPending}
-            >
-              {accountTypes.map(option => (
-                <MenuItem
-                  key={option}
-                  value={option}
-                >
-                  {option}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-        </DialogContent>
-        <DialogActions sx={{ justifyContent: account ? 'space-between' : 'flex-end' }}>
-          {account && (
+      <MainForm
+        button={
+          account ? (
             <IconButton
-              onClick={handleDelete}
-              color="error"
-              type="button"
-              disabled={isPending}
+              aria-label="edit"
+              color="primary"
             >
-              <DeleteOutlineIcon />
+              <EditIcon />
             </IconButton>
-          )}
-          <Box>
+          ) : (
             <Button
-              onClick={handleClose}
-              type="reset"
               variant="outlined"
-              disabled={isPending}
+              endIcon={<AddIcon />}
             >
-              Cancel
+              Budget Account
             </Button>
-            <Button
-              type="submit"
-              variant="contained"
-              disabled={isPending}
+          )
+        }
+        title={account ? `Edit ${account.title}` : 'Create Budget Account'}
+        content={
+          <>
+            <TextField
+              autoFocus
+              required
+              margin="dense"
+              id="title"
+              name="title"
+              label="Title"
+              type="title"
+              fullWidth
+              variant="outlined"
+              value={title}
+              onChange={e => setTitle(e.target.value)}
+            />
+            <TextField
+              id="description"
+              name="description"
+              margin="dense"
+              label="Description (Optional)"
+              type="text"
+              fullWidth
+              variant="outlined"
+              value={description}
+              onChange={e => setDescription(e.target.value)}
+            />
+            <IconSelect
+              icon={icon}
+              setIcon={setIcon}
+            />
+            <TextField
+              id="currentBalance"
+              name="currentBalance"
+              margin="dense"
+              label="Current Balance"
+              type="number"
+              fullWidth
+              variant="outlined"
+              defaultValue={0}
+              required
+              value={currentBalance}
+              onChange={e => setCurrentBalance(e.target.value)}
+            />
+            <TextField
+              id="monthlyLimit"
+              name="monthlyLimit"
+              margin="dense"
+              label="Monthly Limit"
+              type="number"
+              fullWidth
+              variant="outlined"
+              defaultValue={0}
+              required
+              value={monthlyLimit}
+              onChange={e => setMonthlyLimit(e.target.value)}
+            />
+            <FormControl
+              fullWidth
+              margin="dense"
+              variant="outlined"
             >
-              {account ? 'Edit' : 'Create'}
-            </Button>
-          </Box>
-        </DialogActions>
-      </Dialog>
+              <InputLabel id="type-label">Type</InputLabel>
+              <Select
+                id="type"
+                name="type"
+                labelId="type-label"
+                label="Type"
+                defaultValue="Debit"
+                required
+                value={type}
+                onChange={e => setType(e.target.value)}
+              >
+                {accountTypes.map(option => (
+                  <MenuItem
+                    key={option}
+                    value={option}
+                  >
+                    {option}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </>
+        }
+        deleteButton={account ? true : false}
+        accountId={account?.id}
+        accountTitle={account?.title}
+        dialogButtonText={account ? 'Edit' : 'Create'}
+        submitData={{
+          url: account ? `/api/budgetAccount/${account.id}` : '/api/budgetAccount',
+          method: account ? 'PUT' : 'POST',
+          bodyData: { title, description, icon, monthlyLimit, type, currentBalance },
+          successMessage: `Budget Account ${account?.title ? account.title : title} ${account ? 'updated' : 'created'}!`,
+          errorMessage: 'Error while creating/updating budget account',
+          loadingMessage: `Hand tight - we are ${account ? 'updating' : 'creating'} budget account for ya...`,
+        }}
+      />
     </>
   );
 }
--- src/components/molecules/BudgetAccount/BudgetAccountTransactionForm.tsx
+++ src/components/molecules/BudgetAccount/BudgetAccountTransactionForm.tsx
@@ -1,20 +1,8 @@
 'use client';
 import AddIcon from '@mui/icons-material/Add';
-import {
-  Button,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogTitle,
-  FormControl,
-  InputLabel,
-  MenuItem,
-  Select,
-  TextField,
-} from '@mui/material';
-import { useRouter } from 'next/navigation';
-import { FormEvent, useState, useTransition } from 'react';
-import { toast } from 'sonner';
+import { Button, FormControl, InputLabel, MenuItem, Select, TextField } from '@mui/material';
+import { useState } from 'react';
+import MainForm from './Form';
 import { SerializedBudgetAccount } from '@/types/budget';
 
 export default function BudgetAccountTransactionFrom({
@@ -22,208 +10,146 @@ export default function BudgetAccountTransactionFrom({
 }: {
   accounts: SerializedBudgetAccount[];
 }) {
-  const [open, setOpen] = useState(false);
-
   const [fromBudgetAccountId, setFromBudgetAccountId] = useState('');
   const [toBudgetAccountId, setToBudgetAccountId] = useState('');
   const [amount, setAmount] = useState('');
   const [description, setDescription] = useState('');
 
-  const [isPending, startTransition] = useTransition();
-  const router = useRouter();
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    const loadingToastId = toast.loading('Hand tight - we are making transaction');
-
-    startTransition(async () => {
-      try {
-        const response = await fetch('/api/transaction/', {
-          method: 'POST',
-          body: JSON.stringify({
-            fromBudgetAccountId,
-            toBudgetAccountId,
-            amount,
-            description,
-          }),
-        });
-        const responseData = await response.json();
-
-        if (responseData.errorMessage) {
-          toast.error(responseData.errorMessage, { id: loadingToastId });
-        } else if (responseData.success) {
-          router.refresh();
-          toast.success('Transaction completed successfully!', {
-            duration: 5000,
-            id: loadingToastId,
-          });
-          handleClose();
-        } else {
-          toast.error('Something went wrong...', { id: loadingToastId });
-        }
-      } catch (e) {
-        console.error('Error while making transaction', e);
-        toast.error('Something went wrong...', { id: loadingToastId });
-      }
-    });
-  };
-
   return (
     <>
-      <Button
-        onClick={handleClickOpen}
-        endIcon={<AddIcon />}
-        variant="outlined"
-      >
-        Make transaction
-      </Button>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        PaperProps={{
-          component: 'form',
-          onSubmit: handleSubmit,
+      <MainForm
+        button={
+          <Button
+            endIcon={<AddIcon />}
+            variant="outlined"
+          >
+            Make transaction
+          </Button>
+        }
+        title="Make transaction"
+        content={
+          <>
+            <FormControl
+              fullWidth
+              margin="dense"
+              variant="outlined"
+            >
+              <InputLabel id="from-account-label">From Account</InputLabel>
+              <Select
+                id="fromAccount"
+                name="fromAccount"
+                label="From Account"
+                labelId="from-account-label"
+                value={fromBudgetAccountId}
+                onChange={e => setFromBudgetAccountId(e.target.value)}
+                required
+              >
+                {accounts
+                  .filter(account => account.type !== 'SpendingCategory')
+                  .map(account => (
+                    <MenuItem
+                      key={account.id}
+                      value={account.id}
+                    >
+                      {account.title} - {account.type}
+                    </MenuItem>
+                  ))}
+              </Select>
+            </FormControl>
+            <FormControl
+              fullWidth
+              margin="dense"
+              variant="outlined"
+            >
+              <InputLabel id="to-account-label">To Account</InputLabel>
+              <Select
+                id="toAccount"
+                name="toAccount"
+                label="To Account"
+                labelId="to-account-label"
+                value={toBudgetAccountId}
+                onChange={e => setToBudgetAccountId(e.target.value)}
+                required
+              >
+                {accounts
+                  .filter(account => {
+                    const fromAccount = accounts.find(
+                      account => account.id === fromBudgetAccountId,
+                    );
+                    if (!fromAccount) return false;
+
+                    if (
+                      fromAccount.type === 'Income' &&
+                      (account.type === 'Debit' || account.type === 'Credit')
+                    ) {
+                      return true;
+                    }
+
+                    if (
+                      ['Debit', 'Credit', 'Debt'].includes(fromAccount.type) &&
+                      account.type !== 'Income'
+                    ) {
+                      return true;
+                    }
+
+                    if (
+                      fromAccount.type === 'Goal' &&
+                      (account.type === 'Debit' ||
+                        account.type === 'Credit' ||
+                        account.type === 'Debt' ||
+                        account.type === 'Goal')
+                    ) {
+                      return true;
+                    }
+
+                    if (
+                      ['Debit', 'Credit', 'Debt'].includes(fromAccount.type) &&
+                      account.type === 'SpendingCategory'
+                    ) {
+                      return true;
+                    }
+
+                    return false;
+                  })
+                  .map(account => (
+                    <MenuItem
+                      key={account.id}
+                      value={account.id}
+                    >
+                      {account.title} - {account.type}
+                    </MenuItem>
+                  ))}
+              </Select>
+            </FormControl>
+            <TextField
+              label="Amount"
+              type="number"
+              fullWidth
+              margin="normal"
+              value={amount}
+              onChange={e => setAmount(e.target.value)}
+              required
+            />
+
+            <TextField
+              label="Description (Optional)"
+              fullWidth
+              margin="normal"
+              value={description}
+              onChange={e => setDescription(e.target.value)}
+            />
+          </>
+        }
+        dialogButtonText="Submit"
+        submitData={{
+          url: '/api/transaction/',
+          method: 'POST',
+          bodyData: { fromBudgetAccountId, toBudgetAccountId, amount, description },
+          successMessage: 'Transaction completed successfully!',
+          errorMessage: 'Error while making transaction',
+          loadingMessage: 'Hand tight - we are making transaction',
         }}
-      >
-        <DialogTitle>Make transaction</DialogTitle>
-        <DialogContent>
-          <FormControl
-            fullWidth
-            margin="dense"
-            variant="outlined"
-          >
-            <InputLabel id="from-account-label">From Account</InputLabel>
-            <Select
-              id="fromAccount"
-              name="fromAccount"
-              label="From Account"
-              labelId="from-account-label"
-              value={fromBudgetAccountId}
-              onChange={e => setFromBudgetAccountId(e.target.value)}
-              required
-              disabled={isPending}
-            >
-              {accounts
-                .filter(account => account.type !== 'SpendingCategory')
-                .map(account => (
-                  <MenuItem
-                    key={account.id}
-                    value={account.id}
-                  >
-                    {account.title} - {account.type}
-                  </MenuItem>
-                ))}
-            </Select>
-          </FormControl>
-          <FormControl
-            fullWidth
-            margin="dense"
-            variant="outlined"
-          >
-            <InputLabel id="to-account-label">To Account</InputLabel>
-            <Select
-              id="toAccount"
-              name="toAccount"
-              label="To Account"
-              labelId="to-account-label"
-              value={toBudgetAccountId}
-              onChange={e => setToBudgetAccountId(e.target.value)}
-              required
-              disabled={isPending}
-            >
-              {accounts
-                .filter(account => {
-                  const fromAccount = accounts.find(account => account.id === fromBudgetAccountId);
-                  if (!fromAccount) return false;
-
-                  if (
-                    fromAccount.type === 'Income' &&
-                    (account.type === 'Debit' || account.type === 'Credit')
-                  ) {
-                    return true;
-                  }
-
-                  if (
-                    ['Debit', 'Credit', 'Debt'].includes(fromAccount.type) &&
-                    account.type !== 'Income'
-                  ) {
-                    return true;
-                  }
-
-                  if (
-                    fromAccount.type === 'Goal' &&
-                    (account.type === 'Debit' ||
-                      account.type === 'Credit' ||
-                      account.type === 'Debt')
-                  ) {
-                    return true;
-                  }
-
-                  if (
-                    ['Debit', 'Credit', 'Debt'].includes(fromAccount.type) &&
-                    account.type === 'SpendingCategory'
-                  ) {
-                    return true;
-                  }
-
-                  return false;
-                })
-                .map(account => (
-                  <MenuItem
-                    key={account.id}
-                    value={account.id}
-                  >
-                    {account.title} - {account.type}
-                  </MenuItem>
-                ))}
-            </Select>
-          </FormControl>
-          <TextField
-            label="Amount"
-            type="number"
-            fullWidth
-            margin="normal"
-            value={amount}
-            onChange={e => setAmount(e.target.value)}
-            required
-            disabled={isPending}
-          />
-
-          <TextField
-            label="Description (Optional)"
-            fullWidth
-            margin="normal"
-            value={description}
-            onChange={e => setDescription(e.target.value)}
-            disabled={isPending}
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button
-            onClick={handleClose}
-            type="reset"
-            variant="outlined"
-            disabled={isPending}
-          >
-            Cancel
-          </Button>
-          <Button
-            type="submit"
-            variant="contained"
-            disabled={isPending}
-          >
-            Submit
-          </Button>
-        </DialogActions>
-      </Dialog>
+      />
     </>
   );
 }
