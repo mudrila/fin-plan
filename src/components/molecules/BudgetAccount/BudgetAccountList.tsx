cc30ee4056b4c99c954a1a70d2dc4380bdc09705
Kyrylo Klymenko <klimenkomud@gmail.com>
2024-11-14 14:46:01 +0100

    Adjust creation of new budget account UI

--- src/components/molecules/BudgetAccount/BudgetAccountList.tsx
+++ src/components/molecules/BudgetAccount/BudgetAccountList.tsx
@@ -28,7 +28,22 @@ function BudgetAccounts({ accounts, title, initialAccountType, triggerText }: Bu
   const theme = useTheme();
   return (
     <Box>
-      <Typography variant="h6">{title}</Typography>
+      <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Typography variant="h6">{title}</Typography>
+        {accounts.length > 0 && (
+          <BudgetAccountForm
+            initialAccountType={initialAccountType}
+            trigger={
+              <Button
+                variant="outlined"
+                endIcon={<AddIcon />}
+              >
+                {triggerText}
+              </Button>
+            }
+          />
+        )}
+      </Box>
       <Box
         sx={{
           display: 'flex',
@@ -37,10 +52,8 @@ function BudgetAccounts({ accounts, title, initialAccountType, triggerText }: Bu
           flexWrap: 'wrap',
         }}
       >
-        {accounts.map(account => (
+        {accounts.length === 0 ? (
           <BudgetAccountForm
-            key={account.id}
-            account={account}
             trigger={
               <Card
                 variant="outlined"
@@ -53,26 +66,40 @@ function BudgetAccounts({ accounts, title, initialAccountType, triggerText }: Bu
               >
                 <CardActionArea>
                   <CardHeader
-                    avatar={account.icon ? <IconRenderrer iconName={account.icon} /> : undefined}
-                    title={account.title}
-                    subheader={formatCurrency(account.monthlyLimit)}
+                    avatar={<AddIcon />}
+                    title={`New ${triggerText}`}
                   />
                 </CardActionArea>
               </Card>
             }
           />
-        ))}
-        <BudgetAccountForm
-          initialAccountType={initialAccountType}
-          trigger={
-            <Button
-              variant="outlined"
-              endIcon={<AddIcon />}
-            >
-              {triggerText}
-            </Button>
-          }
-        />
+        ) : (
+          accounts.map(account => (
+            <BudgetAccountForm
+              key={account.id}
+              account={account}
+              trigger={
+                <Card
+                  variant="outlined"
+                  sx={{
+                    cursor: 'pointer',
+                    '&:hover': {
+                      boxShadow: theme.shadows[4],
+                    },
+                  }}
+                >
+                  <CardActionArea>
+                    <CardHeader
+                      avatar={account.icon ? <IconRenderrer iconName={account.icon} /> : undefined}
+                      title={account.title}
+                      subheader={formatCurrency(account.monthlyLimit)}
+                    />
+                  </CardActionArea>
+                </Card>
+              }
+            />
+          ))
+        )}
       </Box>
     </Box>
   );
